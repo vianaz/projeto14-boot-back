a5ff741012792abfc7027d8896e94ad04dbe52e5
marcos.asdes <marco.engc@poli.ufrj.br>
2022-05-15 17:27:22 -0300

    fix authenticationRouter.js and validateSignIN.js

--- routes/authenticationRouter.js
+++ routes/authenticationRouter.js
@@ -2,9 +2,9 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 import validateUser from './../middleware/validateUser.js';
-import isUserUnique from './../middleware/isSingleUser.js';
+import isSingleUser from './../middleware/isSingleUser.js';
 import userExists from './../middleware/userExists.js';
-import validateSignin from './../middleware/validateSignin.js';
+import validateSignIn from './../middleware/validateSignIn.js';
 import isUserOnline from './../middleware/isUserOnline.js';
 
 import { signUp, signIn } from './../controllers/authenticationController.js';
@@ -14,6 +14,6 @@ dotenv.config();
 const authenticationRouter = express.Router();
 
 authenticationRouter.post(process.env.SIGNUP, validateUser, isSingleUser, signUp);
-authenticationRouter.post(process.env.SIGNIN, userExists, validateSignin, isUserOnline, signIn);
+authenticationRouter.post(process.env.SIGNIN, userExists, validateSignIn, isUserOnline, signIn);
 
 export default authenticationRouter;
