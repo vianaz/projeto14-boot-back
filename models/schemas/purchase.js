4b6c7618233e4b56b968951316378bf0dad7efc0
pedro imbrech <elprogramadorpedro@gmail.com>
2022-05-19 16:06:43 -0300

    rev

--- models/schemas/purchase.js
+++
@@ -1,6 +0,0 @@
-import Joi from 'joi';
-
-export const purchaseSchema = Joi.object({
-    items: Joi.array().items(Joi.object({ product_id: Joi.string(), volume: Joi.number() })).required(),
-    amount: Joi.number().required()
-});
